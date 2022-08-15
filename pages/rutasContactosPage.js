e773c93554ab8743a6bfbbea6c1f4d171ace78cb
Ismael-devTR <dev.tristan.romero@gmail.com>
2022-08-15 11:49:47 -0500

    feature/nesting-routes

---
+++ pages/rutasContactosPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RutasContactosPage() {
+  return (
+    <div>
+        <ul>
+            <li>Email: myemmail@example.com</li>
+            <li>Twitter: @Myusername</li>
+            <li>Intagram: myUserName</li>
+        </ul>
+    </div>
+  )
+}
+
+export default RutasContactosPage
---
+++ pages/rutasIndexPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RutasIndexPage() {
+  return (
+    <div>THIS IS HOME PAGE.</div>
+  )
+}
+
+export default RutasIndexPage
